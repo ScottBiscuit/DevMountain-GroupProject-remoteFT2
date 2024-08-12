988a6a429b08fd1e928de8a786d894c8d22f816a
Scott Johnstone <scottajohnstone@gmail.com>
2024-08-12 14:42:27 -0500

    initial commit - adding components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
+import ReviewCarousel from '../components/ReviewCarousel'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Card className="text-center">
+      <Card.Img src="../images/game_background.jpg" alt="Card image" className='opacity-50'/>
+      <Card.ImgOverlay>
+        <ReviewCarousel />
+      </Card.ImgOverlay>
+      </Card>
+    </>
   )
 }
