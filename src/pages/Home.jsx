68976497f4725b9136f4efee58a738d90b2e7ecd
Scott Johnstone <scottajohnstone@gmail.com>
2024-08-12 16:50:36 -0500

    added placeholders for multiple components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,12 @@ export default function Home() {
   return (
     <>
       <Card className="text-center">
+        {/* TODO add BG img */}
       <Card.Img src="../images/game_background.jpg" alt="Card image" className='opacity-50'/>
       <Card.ImgOverlay>
+        {/* TODO pull top rated reviews into ReviewCarousel */}
         <ReviewCarousel />
+        {/* TODO add Most Recent Posts Cards */}
       </Card.ImgOverlay>
       </Card>
     </>
