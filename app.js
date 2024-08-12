ba1af560f697d21432cb167c00464ed149bdf76b
Joseph Billings <josephbillings.gb@gmail.com>
2024-08-12 16:02:45 -0600

    added express and created endpoints for login and creating new users

---
+++ app.js
@@ -0,0 +1,68 @@
+import express from "express";
+import session from "express-session";
+import morgan from "morgan";
+import ViteExpress from "vite-express";
+import { User, Review, Tag, Image, WishlistItem } from "./src/model.js";
+import { Op } from "sequelize";
+
+const app = express();
+const port = 8000;
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extend: false }));
+app.use(express.json());
+app.use(
+  session({ secret: "ssshhhhh", saveUninitialized: true, resave: false })
+);
+
+ViteExpress.config({ printViteDevServerHost: true });
+
+function loginRequired(req, res, next) {
+  if (!req.session.userId) {
+    res.status(401).json({ error: "Unauthorized" });
+  } else {
+    next();
+  }
+}
+
+//____________________________START API Endpoints__________________________//
+
+//login
+app.post("/api/auth", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    where: { email: email },
+  });
+  if (user && user.password === password) {
+    req.session.userId = user.userId;
+    res.json({ success: true, userId: req.session.userId });
+  } else {
+    res.json({ success: false });
+  }
+});
+
+//create new user
+app.post("/api/user", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const userEmail = await User.findOne({
+    where: { email: email },
+  });
+
+  const username = await User.findOne({
+    where: { username: username },
+  });
+
+  if (!userEmail && !username) {
+    const user = await User.create({
+      username: username,
+      email: email,
+      password: password,
+    });
+    res.json(user);
+  } else if (userEmail) {
+    res.json({ error: "Email already in use." });
+  } else {
+    res.json({ error: "Username already in use." });
+  }
+});
