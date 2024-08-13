ee83d0e442184d8600eca613c87aa022cd05d531
Scott Johnstone <scottajohnstone@gmail.com>
2024-08-13 11:06:15 -0500

    added SJ db details, added MyInfoCard to User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MyReviewsCards from '../components/MyReviewsCards';
 import MyWishlist from '../components/MyWishlist';
+import MyInfoCard from '../components/MyInfoCard'
 
 export default function User() {
   
   return (
    <>
+    <MyInfoCard />
     <MyReviewsCards />
     <MyWishlist />
    </>
