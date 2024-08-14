484e35b252ba489b58c929b817c995cd8bd8acf0
Joseph Billings <josephbillings.gb@gmail.com>
2024-08-14 14:13:32 -0600

    fixed user ref

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import { Button, Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function MainNav({ brand }) {
   const navigate = useNavigate();
@@ -20,49 +20,51 @@ export default function MainNav({ brand }) {
 
   const handleLogout = async (e) => {
     e.preventDefault();
-    const res= await axios.post('/api/logout');
-    if (res.data.success){
-      setUser(null)
-      navigate('/')
+    const res = await axios.post("/api/logout");
+    if (res.data.success) {
+      setUser(null);
+      navigate("/");
     }
-  }
+  };
   return user ? (
-  // return (
-    <Navbar expand="lg" className="bg-primary navbar-dark" sticky='top'>
+    // return (
+    <Navbar expand="lg" className="bg-primary navbar-dark" sticky="top">
       <Container fluid>
-        <Navbar.Brand href='/'>{brand}</Navbar.Brand>
+        <Navbar.Brand href="/">{brand}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="justify-content-end ms-auto text-end">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/locations">Locations</Nav.Link>
             <NavDropdown title="User" id="basic-nav-dropdown">
-              <NavDropdown.Item href="user" >My Info</NavDropdown.Item>
-              <NavDropdown.Item href="user" >My Reviews</NavDropdown.Item> 
-              <NavDropdown.Item href="user" >Wislist</NavDropdown.Item>  
+              <NavDropdown.Item href="user">My Info</NavDropdown.Item>
+              <NavDropdown.Item href="user">My Reviews</NavDropdown.Item>
+              <NavDropdown.Item href="user">Wislist</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item onClick={handleLogout} >Log Out?</NavDropdown.Item>
+              <NavDropdown.Item onClick={handleLogout}>
+                Log Out?
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  ):(
-    <Navbar expand="lg" className="bg-primary navbar-dark" sticky='top'>
-    <Container fluid>
-      <Navbar.Brand href='/'>{brand}</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="justify-content-end ms-auto text-end">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/locations">Locations</Nav.Link>
-          <NavDropdown title="User" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/login" >Log In</NavDropdown.Item>
-            <NavDropdown.Item href="/register" >Register</NavDropdown.Item> 
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  )
+  ) : (
+    <Navbar expand="lg" className="bg-primary navbar-dark" sticky="top">
+      <Container fluid>
+        <Navbar.Brand href="/">{brand}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end ms-auto text-end">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/locations">Locations</Nav.Link>
+            <NavDropdown title="User" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/login">Log In</NavDropdown.Item>
+              <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
