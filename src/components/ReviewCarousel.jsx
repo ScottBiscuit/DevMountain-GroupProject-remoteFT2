c7970e5d0ee5050d852b816f4c47fa7c864bf33f
Joseph Billings <josephbillings.gb@gmail.com>
2024-08-15 10:29:34 -0600

    added email validation and additional error handling for registration

--- src/components/ReviewCarousel.jsx
+++ src/components/ReviewCarousel.jsx
@@ -1,24 +1,24 @@
-import { Carousel, Image } from 'react-bootstrap';
+import { Carousel, Image } from "react-bootstrap";
 
 export default function ReviewCarousel() {
   return (
-    <Carousel className='bg-dark-subtle opacity-75'>
+    <Carousel className="bg-dark-subtle w-50">
       <Carousel.Item>
-        <Image src='../images/new_york_times_square.jpg'/>
+        <Image src="../images/new_york_times_square.jpg" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <Image src='../images/new_york_times_square.jpg'/>
+        <Image src="../images/new_york_times_square.jpg" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <Image src='../images/new_york_times_square.jpg'/>
+        <Image src="../images/new_york_times_square.jpg" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
