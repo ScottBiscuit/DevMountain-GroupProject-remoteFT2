9725b5ae697552da4a58f7d22c0386bcd7a985f5
Scott Johnstone <scottajohnstone@gmail.com>
2024-08-15 19:58:18 -0500

    review carousel now pulls data from database

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
 import { Card, Container } from 'react-bootstrap'
 import ReviewCarousel from '../components/ReviewCarousel';
 import RecentReviewsCards from '../components/RecentReviewsCards';
+import { useLoaderData } from 'react-router-dom';
 
 export default function Home() {
+  const [reviews, setReviews] = useState(useLoaderData().reviews);
 
   return (
   
       <Card className='h-100'>
       <Card.Img src="../images/world_map_pins.jpg" alt="World map with location pins" className='opacity-50 vh-100' />
       <Card.ImgOverlay>
-        <ReviewCarousel />
+        <ReviewCarousel 
+          reviews={reviews} 
+          setReviews={setReviews}
+        />
         <RecentReviewsCards />
       </Card.ImgOverlay>
       </Card>
