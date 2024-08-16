0245aae4e675080ca8270c938b0a327794b10a89
Joseph Billings <josephbillings.gb@gmail.com>
2024-08-16 16:17:38 -0600

    added edit button, no functional; corrected endpoint to call up reviews for a wishlist

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Button } from "react-bootstrap";
 
-function ReviewCard({ review }) {
+function ReviewCard({ review, user }) {
   console.log(review);
 
-  return (
+  return user && user.userId === review.userId ? (
+    <Col>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>{review.locationName}</Card.Title>
+          <Card.Subtitle className="text-muted">
+            {review.city}, {review.state} {review.country}
+          </Card.Subtitle>
+          <Card.Subtitle className="text-muted">
+            {review.streetAddress}
+          </Card.Subtitle>
+          <Card.Text>{review.reviewContent}</Card.Text>
+          <Card.Text>{review.likeCount}</Card.Text>
+          <Button>Edit Review</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  ) : (
     <Col>
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
